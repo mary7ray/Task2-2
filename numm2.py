0b16f747e95127849ac7ce657b73badd018d3257
pineapple79 <fun.goul.109@gmail.com>
2019-11-12 18:28:36 +0600

    Task1-2

---
+++ numm2.py
@@ -0,0 +1,8 @@
+students = int(input("enter the number of students:"))
+apples = int(input("enter the number of Apples:"))
+
+a = apples/students
+b = apples % students
+
+print(int(a))
+print(int(b))
